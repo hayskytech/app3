f7dbd3d82286e4a4c48b8f16dacf611d8a1b5034
sufyan <mdsufyan7@gmail.com>
2023-10-10 11:43:49 +0530

    first day

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
-import MainMenu from './comps/MainMenu'
-import 'semantic-ui-css/semantic.min.css'
 
 export default function App() {
   return (
     <div>
-      <MainMenu />
+      hai
+      <h1>hai</h1>
+      <h2>hello</h2>
+      <hr />
+      <br />
+      hai
+      <b>hai</b>
+      <i>hai</i>
+      <ins>hello</ins>
+
+      <img src="https://assets.editorial.aetnd.com/uploads/2011/06/taj-mahal-gettyimages-463924915.jpg" alt="" width='300' />
+
+      <h1>askdb</h1>
     </div>
   )
 }
